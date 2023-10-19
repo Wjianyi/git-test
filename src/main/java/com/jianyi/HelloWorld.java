556929ab3d35679fd9e57bba30a9de6cda59a85e
jianyi <jianyi@wang.com>
2023-10-19 12:38:58 +0800

    master commit3

--- src/main/java/com/jianyi/HelloWorld.java
+++ src/main/java/com/jianyi/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
         System.out.println("hello git hot-fix");
         System.out.println(" master test 2");
         System.out.println(" hot-fix test ");
+        System.out.println(" push test ");
 
 
     }
